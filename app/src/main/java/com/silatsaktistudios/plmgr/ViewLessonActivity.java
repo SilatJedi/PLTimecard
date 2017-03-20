a4f7ec331ea2f22e63d6c28ce8a7967f71824db9
James Anderson <silatjedi@gmail.com>
2017-03-20 17:29:08 -0600

    added view lesson activity

---
+++ app/src/main/java/com/silatsaktistudios/plmgr/ViewLessonActivity.java
@@ -0,0 +1,14 @@
+package com.silatsaktistudios.plmgr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ViewLessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_lesson);
+    }
+}
