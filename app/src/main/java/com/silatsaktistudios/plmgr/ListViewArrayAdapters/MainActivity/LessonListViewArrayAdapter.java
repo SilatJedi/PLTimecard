c201780b5feff4898624d8def7203fc36ba766fe
SilatJedi <Silatjedi@gmail.com>
2017-04-09 10:39:30 -0600

    adapter now accepts object list instead of multiple string arrays

--- app/src/main/java/com/silatsaktistudios/plmgr/ListViewArrayAdapters/MainActivity/LessonListViewArrayAdapter.java
+++ app/src/main/java/com/silatsaktistudios/plmgr/ListViewArrayAdapters/MainActivity/LessonListViewArrayAdapter.java
@@ -8,9 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.silatsaktistudios.plmgr.Models.Lesson;
 import com.silatsaktistudios.plmgr.R;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by james on 1/1/17.
@@ -18,20 +20,21 @@ import java.util.Date;
 
 public class LessonListViewArrayAdapter extends ArrayAdapter<String> {
     private Context context;
-    private String[] names;
-    private Date[] dates;
-    private boolean[] showedUps, eligibles, makeUps;
+//    private String[] names;
+//    private Date[] dates;
+//    private boolean[] showedUps, eligibles, makeUps;
+    private List<Lesson> lessons;
 
 
-    public LessonListViewArrayAdapter(Context context, String[] names, Date[] dates,
-                                      boolean[] showedUps, boolean[] eligibles, boolean[] makeUps) {
-        super(context, R.layout.timecard_list_item, names);
+    public LessonListViewArrayAdapter(Context context, List<Lesson> lessons) {
+        super(context, R.layout.timecard_list_item, new String[lessons.size()]);//workaround, using the string array to define number of list items nothing shows up with out it
         this.context = context;
-        this.names = names;
-        this.dates = dates;
-        this.showedUps = showedUps;
-        this.eligibles = eligibles;
-        this.makeUps = makeUps;
+//        this.names = names;
+//        this.dates = dates;
+//        this.showedUps = showedUps;
+//        this.eligibles = eligibles;
+//        this.makeUps = makeUps;
+        this.lessons = lessons;
     }
 
     @Override
@@ -45,42 +48,48 @@ public class LessonListViewArrayAdapter extends ArrayAdapter<String> {
             rowView.setBackgroundColor(Color.LTGRAY);
         }
 
+        Lesson lesson = lessons.get(position);
+
         TextView studentName = (TextView) rowView.findViewById(R.id.timecardItemStudentName);
-        studentName.setText(names[position]);
+//        studentName.setText(names[position]);
+        studentName.setText(lesson.getStudentName());
 
         TextView dateTime = (TextView) rowView.findViewById(R.id.timecardItemDateTime);
         TextView showedUp = (TextView)rowView.findViewById(R.id.timecardShowedStatus);
         TextView eligible = (TextView)rowView.findViewById(R.id.timecardEligibleStatus);
         TextView makeUp = (TextView)rowView.findViewById(R.id.makeUpStatus);
 
-        if (!names[position].equals("No Lessons Found")) {
-            if (showedUps[position]) {
+//        if (!names[position].equals("No Lessons Found")) {
+//            if (showedUps[position]) {
+            if (lesson.didShowUp()) {
                 showedUp.setText("Showed Up");
                 eligible.setText("");
             } else {
                 showedUp.setText("No Show");
                 eligible.setVisibility(View.VISIBLE);
 
-                if (eligibles[position]) {
+//                if (eligibles[position]) {
+                if(lesson.isEligible()) {
                     eligible.setText("Eligible");
                 } else {
                     eligible.setText("Not Eligible");
                 }
             }
 
-            if (makeUps[position]) {
+//            if (makeUps[position]) {
+            if(lesson.isMakeUp()) {
                 makeUp.setVisibility(View.VISIBLE);
                 makeUp.setText("Make Up Lesson");
             }
 
-            dateTime.setText(android.text.format.DateFormat.format("EEE d MMM, yyyy h:mm a", dates[position]));
-        }
-        else {
-            showedUp.setText("");
-            eligible.setText("");
-            makeUp.setText("");
-            dateTime.setText("");
-        }
+            dateTime.setText(android.text.format.DateFormat.format("EEE d MMM, yyyy h:mm a", /*dates[position]*/lesson.getDate()));
+//        }
+//        else {
+//            showedUp.setText("");
+//            eligible.setText("");
+//            makeUp.setText("");
+//            dateTime.setText("");
+//        }
         return rowView;
     }
 }
