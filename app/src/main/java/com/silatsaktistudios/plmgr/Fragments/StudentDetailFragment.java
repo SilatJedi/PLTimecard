f1183941f7f4e7277aa64de203b704eeea14e2b2
James Anderson <silatjedi@gmail.com>
2017-04-13 16:41:22 -0600

    cleared code stubs

--- app/src/main/java/com/silatsaktistudios/plmgr/Fragments/StudentDetailFragment.java
+++ app/src/main/java/com/silatsaktistudios/plmgr/Fragments/StudentDetailFragment.java
@@ -23,14 +23,6 @@ import com.silatsaktistudios.plmgr.R;
  * create an instance of this fragment.
  */
 public class StudentDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -38,16 +30,9 @@ public class StudentDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StudentDetailFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static StudentDetailFragment newInstance(String param1, String param2) {
+    public static StudentDetailFragment newInstance() {
         StudentDetailFragment fragment = new StudentDetailFragment();
         return fragment;
     }
